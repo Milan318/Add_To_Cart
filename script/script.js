f280c09d1bd4a3fe04f9b125a42f3828f1dbfb51
Milan318 <milanmandaviya7495@gmail.com>
2025-04-07 17:55:07 +0530

    done

---
+++ script/script.js
@@ -0,0 +1,9 @@
+let addBtn = document.querySelectorAll('.bi-cart-fill');
+let card = document.querySelectorAll('.card')
+let data = [];
+
+addBtn.forEach((btn,index)=>{
+    btn.addEventListener("click",()=>{
+        let title = card [index].querySelector(".card-title")
+    })
+})
